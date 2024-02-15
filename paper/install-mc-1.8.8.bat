886f994490078ded6ce6c8f2d5bece2eb83f9092
thanh20VN <95450843+thanh20VN@users.noreply.github.com>
2024-02-15 21:09:19 +0700

    Add files via upload

---
+++ paper/install-mc-1.8.8.bat
@@ -0,0 +1,13 @@
+@echo off
+title Install Minecaft 1.8.8 paper
+echo Folder Server name
+set/p "sn=>"
+mkdir %sn%
+cd %sn%
+curl -s -L -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar
+cls
+curl -s -L -o eula.txt https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/eula.txt
+curl -s -L -o start.bat https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/start.bat
+cd ..
+del install-mc-1.8.8.bat
+exit

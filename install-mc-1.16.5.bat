8b1e13a6db0baf8429912f286fb82997287b4ddd
thanh20VN <95450843+thanh20VN@users.noreply.github.com>
2024-02-15 20:59:44 +0700

    Update install-mc-1.16.5.bat

--- install-mc-1.16.5.bat
+++ install-mc-1.16.5.bat
@@ -1,11 +1,13 @@
 @echo off
 title Install Minecaft 1.16.5 paper
-mkdir server
-cd server
+echo Folder Server name
+set/p "sn=>"
+mkdir %sn%
+cd %sn%
 curl -s -L -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
 cls
-
-
+curl -s -L -o eula.txt https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/eula.txt
+curl -s -L -o start.bat https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/eula.txt
 cd ..
 del install-mc-1.16.5.bat
 exit

4d022548bc65c5e45727618d29a75704b0ea6617
thanh20VN <95450843+thanh20VN@users.noreply.github.com>
2024-02-15 21:01:00 +0700

    Update install-mc-1.16.5.bat

--- install-mc-1.16.5.bat
+++ install-mc-1.16.5.bat
@@ -7,7 +7,7 @@ cd %sn%
 curl -s -L -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.16.5/builds/794/downloads/paper-1.16.5-794.jar
 cls
 curl -s -L -o eula.txt https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/eula.txt
-curl -s -L -o start.bat https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/eula.txt
+curl -s -L -o start.bat https://github.com/thanh20VN/Automatic_Install_Server_Minecraft/raw/main/necessary_files/start.bat
 cd ..
 del install-mc-1.16.5.bat
 exit
